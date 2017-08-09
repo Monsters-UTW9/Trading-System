eec118ea7e2eb0a619d34983c42e067380288b38
Amal Tarek <dr.amaltarek_467@fci.helwan.edu.eg>
2017-08-09 21:02:09 +0200

    MVC UTW 9 Project

---
+++ UTW9Project/UTW9Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UTW9Project.MvcApplication" Language="C#" %>
---
+++ UTW9Project/UTW9Project/Scripts/MainJs.js
@@ -0,0 +1,46 @@
+﻿$('.form').find('input, textarea, select').on('keyup blur focus', function (e) {
+
+    var $this = $(this),
+        label = $this.prev('label');
+
+    if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+        } else {
+            label.removeClass('highlight');
+        }
+    } else if (e.type === 'focus') {
+
+        if ($this.val() === '') {
+            label.removeClass('highlight');
+        }
+        else if ($this.val() !== '') {
+            label.addClass('highlight');
+        }
+    }
+
+});
+
+$('.profile-img-container img').click(function () {
+    $('#image').click();
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $('#profileImg')
+                .attr('src', e.target.result);
+
+        };
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
